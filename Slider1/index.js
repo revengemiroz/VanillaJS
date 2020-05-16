9682a3fbfeb13006e4ff855d0df4c499023b2712
revengemiroz <mirozxy@gmail.com>
2020-05-17 00:31:50 +0545

    added slider

--- Slider1/index.js
+++ Slider1/index.js
@@ -14,7 +14,7 @@ newImg.onload = function () {
     slider.src = this.src;
     console.log(`width : ${this.width},height : ${this.height}`)
 
-    dimension(this.width, this.height)
+    dimension(this.width, this.height, 10, 5, 300, 600, 800)
 }
 newImg.src = `${imagePath[0]}`
 
@@ -22,30 +22,64 @@ imgId.innerText = '1'
 let counter = 0;
 
 function dimension(w, h) {
-    if (w == 4912 && h == 7360) {
-        container.style.width = `${w / 10}px`
-        container.style.height = `${h / 10}px`
-    }
+    if (window.screen.width < 500) {
+        if (w == 4912 && h == 7360) {
+            container.style.width = `${w / 12.6}px`
+            container.style.height = `${h / 10}px`
+        }
 
-    else if (w > 4800 && h > 3500) {
-        container.style.width = `${w / 5}px`
-        container.style.height = `${h / 5}px`
-    }
+        else if (w > 4800 && h > 3500) {
+            container.style.width = `${w / 15.5}px`
+            container.style.height = `${h / 11}px`
+        }
 
-    else if (w < 800 && w < 800) {
-        container.style.width = `${w}px`
-        container.style.height = `${h}px`
-    }
+        else if (w == 750 && h == 750) {
+            container.style.width = `${w - 370}px`
+            container.style.height = `${h - 150}px`
+        }
 
-    else if (w > 600 && w < 1200) {
-        container.style.width = `${w - 300}px`
-        container.style.height = `${h - 300}px`
-    }
+        else if (w < 800 && w < 800) {
+            container.style.width = `${w - 270}px`
+            container.style.height = `${h - 200}px`
+        }
 
+        else if (w > 600 && w < 1200) {
+            container.style.width = `${w - 700}px`
+            container.style.height = `${h - 460}px`
+        }
+
+        else {
+            container.style.width = `${w - 850}px`
+            container.style.height = `${h - 1020}px`
+        }
+    }
     else {
-        container.style.width = `${w - 600}px`
-        container.style.height = `${h - 800}px`
+        if (w == 4912 && h == 7360) {
+            container.style.width = `${w / 10}px`
+            container.style.height = `${h / 10}px`
+        }
+
+        else if (w > 4800 && h > 3500) {
+            container.style.width = `${w / 5}px`
+            container.style.height = `${h / 5}px`
+        }
+
+        else if (w < 800 && w < 800) {
+            container.style.width = `${w}px`
+            container.style.height = `${h}px`
+        }
+
+        else if (w > 600 && w < 1200) {
+            container.style.width = `${w - 300}px`
+            container.style.height = `${h - 300}px`
+        }
+
+        else {
+            container.style.width = `${w - 600}px`
+            container.style.height = `${h - 800}px`
+        }
     }
+
 }
 
 function inc(count) {
