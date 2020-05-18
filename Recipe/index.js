6d2dcbd2956725f7f295b08a370f366f8cb8d7d1
revengemiroz <mirozxy@gmail.com>
2020-05-19 00:55:48 +0545

    Recipe

---
+++ Recipe/index.js
@@ -0,0 +1,169 @@
+const menu = [
+    {
+        id: 1,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+        id: 2,
+        title: "diner double",
+        category: "lunch",
+        price: 13.99,
+        img: "./images/item-2.jpeg",
+        desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+        id: 3,
+        title: "godzilla milkshake",
+        category: "shakes",
+        price: 6.99,
+        img: "./images/item-3.jpeg",
+        desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+        id: 4,
+        title: "country delight",
+        category: "breakfast",
+        price: 20.99,
+        img: "./images/item-4.jpeg",
+        desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+        id: 5,
+        title: "egg attack",
+        category: "lunch",
+        price: 22.99,
+        img: "./images/item-5.jpeg",
+        desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+        id: 6,
+        title: "oreo dream",
+        category: "shakes",
+        price: 18.99,
+        img: "./images/item-6.jpeg",
+        desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+    {
+        id: 7,
+        title: "bacon overflow",
+        category: "breakfast",
+        price: 8.99,
+        img: "./images/item-7.jpeg",
+        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    },
+    {
+        id: 8,
+        title: "american classic",
+        category: "lunch",
+        price: 12.99,
+        img: "./images/item-8.jpeg",
+        desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    },
+    {
+        id: 9,
+        title: "quarantine buddy",
+        category: "shakes",
+        price: 16.99,
+        img: "./images/item-9.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+    {
+        id: 10,
+        title: "steak dinner",
+        category: "dinner",
+        price: 39.99,
+        img: "./images/item-10.jpeg",
+        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
+];
+
+
+const menusHolder = document.querySelector('.menus')
+const allMenus = document.querySelector('.allMenus')
+
+
+function displayMenuButtons(menu) {
+    let uniq = ['all']
+    for (const [keys, values] of Object.entries(menu)) {
+        uniq.push(values.category)
+
+    }
+    uniq = [...new Set(uniq)]
+
+    uniq.map(a => {
+        const word = captialize(a)
+        const html = `<button class='menuCategory'>${word}</button>`
+        menusHolder.innerHTML += html
+    })
+
+}
+
+function displayMenusDescription(menu) {
+    allMenus.innerHTML = ''
+    for (const [keys, values] of Object.entries(menu)) {
+        const html = `
+        <article>
+            <div class='image'>
+                <img src='${values.img}'></img>
+            </div>
+            
+            <div class='details'>
+            
+                <div class='title'>
+                <p>${captialize(values.title)}</p>
+                <span>$ ${values.price}</span>
+                </div>
+                
+                <div class='description'>
+                <p>${values.desc}</p>
+                </div>
+            </div>
+
+            </article>
+            `
+
+        allMenus.innerHTML += html
+    }
+}
+
+
+displayMenusDescription(menu)
+displayMenuButtons(menu)
+
+const allButtons = document.querySelectorAll('.menuCategory')
+
+allButtons.forEach(button => {
+    const text = button.textContent
+    console.log(text)
+    click(button, text)
+})
+
+function click(a, text) {
+    a.addEventListener('click', () => {
+
+        if (captialize(text) == 'All') {
+            displayMenusDescription(menu)
+        }
+        else {
+
+            // stackOverflow
+            Object.filter = (menu, predicate) =>
+                Object.keys(menu)
+                    .filter(key => predicate(menu[key]))
+                    .reduce((res, key) => (res[key] = menu[key], res), {});
+
+            const updatedMenu = Object.filter(menu, s => captialize(s.category) == text)
+            console.warn(updatedMenu)
+            displayMenusDescription(updatedMenu)
+        }
+    })
+}
+
+function captialize(a) {
+    return a.charAt(0).toUpperCase() + a.slice(1)
+}
+
